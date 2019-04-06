484527a9f072bef24a90e4127e73607d40812ab0
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-05 22:01:49 -0400

    add generateToken fx, add login post fx

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const users = require('./../database/users/users');
+const secret = require('./../config/parole').jwtSecret;
+
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -15,7 +18,7 @@ function register(req, res) {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
-  
+
   users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -25,8 +28,38 @@ function register(req, res) {
     });
 }
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, secret, options)
+}
+
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  console.log(username);
+  users.findBy({ username })
+    .first()
+    .then(user => {
+      console.log(user)
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        console.log(token);
+        res.status(200).json({
+          token, message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
